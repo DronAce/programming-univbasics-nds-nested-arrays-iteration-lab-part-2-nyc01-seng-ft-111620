c6e8237ea357238789575450e758a02a6629fa0c
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-30 17:59:20 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,11 @@
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  for i in 0...src.size
+    for y in 0...src[i].size
+      if src[i][y] < 0
+        puts src[i][y]
+      end
+    end
+  end
 end
